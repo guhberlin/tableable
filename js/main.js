836db271a55a4133749f2fe0f55890abed51880a
manuelpiesold <manuel@piesold.com>
2014-08-10 18:08:36 +0200

    basic changes for a more custom layout

---
+++ js/main.js
@@ -0,0 +1,42 @@
+
+$.fn.isOnScreen = function(){
+
+    var win = $(window);
+
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+
+};
+
+$(document).ready(function($) {
+
+
+    $(document).scroll(function(event) {
+
+        var top = $(window).scrollTop();
+
+        if ( $('#bigHead').isOnScreen() ) {
+
+            $('#smallHead').css('display', 'none');
+            $('#sidebar').css({ position: 'relative', right: 10 });
+
+        } else {
+
+            $('#smallHead').css({ top: top, display: 'block' });
+            $('#sidebar')
+                .css({ position: 'absolute', top: ( top - $('#bigHead').height() ), right: 10 });
+
+        }
+    });
+
+});
