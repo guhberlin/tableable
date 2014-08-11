61b966e44d08dbdf93fc3a91e106a1c72657277c
manuelpiesold <manuel@piesold.com>
2014-08-11 13:51:00 +0200

    added basic tests for sorter

--- README.md
+++ README.md
@@ -68,7 +68,7 @@ After filtering, sorting and pagechanging the plugin will trigger events so you 
 
 - [x] add functionality for sorting table
 - [x] add triggers for callbacks like `$('element').on('page-changed')`
-- [ ] finish testcases for sorter
+- [x] finish testcases for sorter
 - [ ] optional sorting on init
 
 ## History
--- tests/suites/sorter.js
+++ tests/suites/sorter.js
@@ -3,6 +3,20 @@ casper.test.begin( 'tests: sorter', function( test ) {
 
  	casper.start( 'tests/test.html' )
 
+    .then( function() {
+        test.comment( '> first column sortable' );
+        this.click( '#sorter table thead tr th:first-child' );
+        test.assertSelectorHasText( '#sorter table tbody tr td:first-child', 'abc' );
+        this.click( '#sorter table thead tr th:first-child' );
+        test.assertSelectorHasText( '#sorter table tbody tr td:first-child', 'ghi' );
+    })
+
+    .then( function() {
+        test.comment( '> second column not sortable' );
+        this.click( '#sorter table thead tr th:last-child' );
+        test.assertSelectorHasText( '#sorter table tbody tr td:last-child', '789' );
+    })
+
  	.run( function() { test.done(); });
 
 });
