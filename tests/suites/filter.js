8b8e71655de2e1eaf8628de1559a830546b997e7
skeln <skeln@gmx.de>
2014-12-17 15:00:13 +0100

    added testcases for filter; check if elements are visible or not

--- tests/suites/filter.js
+++ tests/suites/filter.js
@@ -5,23 +5,30 @@ casper.test.begin( 'tests: filter', function( test ) {
 
 	.then( function() {
 		test.comment( '> table' );
-		test.assertElementCount( '#filter table tbody tr', 3 );
+		test.assertElementCount( '#filter table tbody tr', 4 );
+		test.assertElementCount( '#filter table tbody tr[data-is-filtered]', 1 );
+		test.assertNotVisible( '#filter table tbody tr[data-is-filtered]' );
+		test.assertVisible( '#filter table tbody tr:not([data-is-filtered])' );
 	})
 
 	.then( function() {
 		test.comment( '> input.filter' );
 
 		this.sendKeys( '#filter .filter', 'abc' );
-		test.assertElementCount( '#filter table tbody tr[data-is-filtered]', 1 );
+		test.assertElementCount( '#filter table tbody tr[data-is-filtered]', 2 );
 
 		this.sendKeys( '#filter .filter', 'abcdef' );
-		test.assertElementCount( '#filter table tbody tr[data-is-filtered]', 3 );
+		test.assertElementCount( '#filter table tbody tr[data-is-filtered]', 4 );
+		test.assertElementCount( '#filter table tbody tr:not([data-is-filtered])', 0 );
+		test.assertNotVisible( '#filter table tbody tr[data-is-filtered]' );
 
 		this.sendKeys( '#filter .filter', null, {reset: true} );
-		test.assertElementCount( '#filter table tbody tr[data-is-filtered]', 0 );
+		test.assertElementCount( '#filter table tbody tr[data-is-filtered]', 1 );
 
 		this.sendKeys( '#filter .filter', 'ABC' );
-		test.assertElementCount( '#filter table tbody tr[data-is-filtered]', 1 );
+		test.assertElementCount( '#filter table tbody tr[data-is-filtered]', 2 );
+		test.assertNotVisible( '#filter table tbody tr[data-is-filtered]' );
+		test.assertVisible( '#filter table tbody tr:not([data-is-filtered])' );
 	})
 
 	.then( function() {
